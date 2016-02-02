6162a22df9722b35dffe99f9367b2b19a11fbc14
NPTPolynomial <chengcao8@gmail.com>
2016-02-01 19:09:20 -0800

    added new database

--- morse_db.sql
+++ morse_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 25, 2016 at 01:51 AM
+-- Generation Time: Feb 02, 2016 at 04:07 AM
 -- Server version: 5.5.29
 -- PHP Version: 5.4.10
 
@@ -31,19 +31,63 @@ CREATE TABLE `board` (
   `from` varchar(10) NOT NULL,
   `to` varchar(10) NOT NULL,
   `message` text NOT NULL,
+  `dial_id` varchar(30) NOT NULL DEFAULT '0',
   PRIMARY KEY (`board_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `board`
 --
 
-INSERT INTO `board` (`board_id`, `from`, `to`, `message`) VALUES
-(1, 'a', 'b', 'hello b'),
-(2, 'b', 'a', 'hello back a'),
-(3, 'c', 'b', 'Hello b, long time no see'),
-(4, 'a', 'b', 'hello'),
-(5, 'a', 'b', 'hello');
+INSERT INTO `board` (`board_id`, `from`, `to`, `message`, `dial_id`) VALUES
+(1, 'a', 'b', 'hello b', '0'),
+(2, 'b', 'a', 'hello back a', '0'),
+(3, 'c', 'b', 'Hello b, long time no see', '0'),
+(4, 'a', 'b', 'hello', '0'),
+(5, 'a', 'b', 'hello', '0'),
+(6, 'b', 'a', 'helolololol', '0'),
+(7, 'c', 'd', 'kugvkjhbljhbl', '2200'),
+(8, 'a', 'b', 'message', '0'),
+(9, 'a', 'b', 'hello', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dialog`
+--
+
+CREATE TABLE `dialog` (
+  `dial_id` varchar(12) NOT NULL,
+  `dial_message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `dialog`
+--
+
+INSERT INTO `dialog` (`dial_id`, `dial_message`) VALUES
+('0', 'Hello!'),
+('1', 'HOW YOU DOING?'),
+('2200', 'Leo is in the way!');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ran`
+--
+
+CREATE TABLE `ran` (
+  `ran_id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` varchar(30) NOT NULL,
+  PRIMARY KEY (`ran_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `ran`
+--
+
+INSERT INTO `ran` (`ran_id`, `message`) VALUES
+(1, 'henry');
 
 -- --------------------------------------------------------
 
@@ -57,8 +101,9 @@ CREATE TABLE `table_a` (
   `instruct` varchar(35) NOT NULL,
   `to` varchar(10) NOT NULL,
   `end` varchar(2) NOT NULL,
+  `dial_id` varchar(20) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -72,22 +117,9 @@ CREATE TABLE `table_b` (
   `instruct` varchar(35) NOT NULL,
   `to` varchar(10) NOT NULL,
   `end` varchar(2) NOT NULL,
+  `dial_id` varchar(20) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
-
---
--- Dumping data for table `table_b`
---
-
-INSERT INTO `table_b` (`date`, `id`, `instruct`, `to`, `end`) VALUES
-('2016-01-24 23:44:38', 1, 'hello', 'a', '1'),
-('2016-01-25 00:11:31', 2, 'a', 'hello', '0'),
-('2016-01-25 00:12:02', 3, 'hello', 'a', '0'),
-('2016-01-25 00:15:34', 4, 'hello', 'a', '0'),
-('2016-01-25 00:23:59', 5, 'hello', 'a', '0'),
-('2016-01-25 00:24:13', 6, 'hello', 'a', '0'),
-('2016-01-25 00:30:00', 7, 'hello', 'a', '0'),
-('2016-01-25 00:30:43', 8, 'hello', 'a', '0');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
