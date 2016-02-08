875e638d43aa0d17c902344210eff203f919fe62
NPTPolynomial <chengcao8@gmail.com>
2016-02-07 20:19:27 -0800

    made a bubble board with ugly colors

--- angulartest.php
+++ angulartest.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+<head>
 <style>
 
 *{
@@ -42,9 +43,67 @@ table tr:nth-child(even) {
 	width: 700px;
 }
 
+#bubble_board{
+	display: none;
+	margin-top: 30px;
+	width: 600px;
+	height: 600px;
+}
+
+.message_left{
+	width: 500px;
+	height: 100px;
+}
+
+.message_right{
+	width: 500px;
+	height: 100px;
+}
+
+.message_arrow{
+	width: 10px;
+}
+
+.message_body_left{
+	width: 180px;	
+	padding: 15px;
+	float: left;
+
+
+	-webkit-border-radius: 0px 77px 77px 77px;
+	-moz-border-radius: 0px 77px 77px 77px;
+	border-radius: 0px 77px 77px 77px;
+	border: 4px solid #FFFF00;
+	background-color:#5FD9D5;
+	-webkit-box-shadow: #B3B3B3 14px 14px 14px;
+	-moz-box-shadow: #B3B3B3 14px 14px 14px;
+	box-shadow: #B3B3B3 14px 14px 14px;
+	
+}
+
+
+.message_body_right{
+	width: 180px;	
+	padding: 15px;
+	float: right;
+
+	-webkit-border-radius: 77px 0px 77px 77px;
+	-moz-border-radius: 77px 0px 77px 77px;
+	border-radius: 77px 0px 77px 77px;
+	border: 4px solid #FFFF00;
+	background-color:#5FD9D5;
+	-webkit-box-shadow: #B3B3B3 14px 14px 14px;
+	-moz-box-shadow: #B3B3B3 14px 14px 14px;
+	box-shadow: #B3B3B3 14px 14px 14px;
+	
+}
+
 
 </style>
 <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+
+</head>
+
 <body>
 
 <div class="board_display" ng-app="myApp" ng-controller="customersCtrl">
@@ -53,25 +112,95 @@ table tr:nth-child(even) {
 	<th>Board_id</th>
 	<th>From</th>
 	<th>Message Type</th>
+	<th>End</th>
 	<th>To</th>
 	<th>Dial_id</th>
 	<th>Dial_message</th>
 	
 	<tr>
-	<td id="loading_sign" class="loadingImg" colspan="6"><img src="img/hex-loader2.gif"></img></td>
+	<td id="loading_sign" class="loadingImg" colspan="7"><img src="img/hex-loader2.gif"></img></td>
 	</tr>
 	
 	<tr ng-repeat="x in names">
 		<td>{{ x.board_id }}</td>
 		<td>{{ x.from }}</td>
 		<td>{{ x.type }}</td>
+		<td>{{ x.end }}</td>
 		<td>{{ x.to }}</td>
 		<td>{{ x.dial_id }}</td>
 		<td>{{ x.dial_message }}</td>
 	</tr>
 </table>
 
+
+
+<div id="bubble_board">
+	
+	<h2> Hi, this is the bubble board </h2>
+	<br />
+	
+	<div ng-repeat="x in names">
+		
+		<div ng-if="x.end == 0">
+		<div class="message_left">
+			<div class="message_arrow">
+				
+			</div>
+			
+				<div class="message_body_left">
+		
+					<td>{{ x.board_id }}</td>
+					<b>{{ x.from }}:</b>
+					<!--<td>{{ x.type }}</td>
+					<td>{{ x.end }}</td>-->
+					<i>@{{ x.to }} </i>
+					<!--<td>{{ x.dial_id }}</td>-->
+					<td>{{ x.dial_message }}</td>
+				</div>
+			</div>
+			
+		</div>
+		
+		
+		
+		
+		
+		
+		
+		<div ng-if="x.end == 1">
+		<div class="message_right">
+			<div class="message_arrow">
+				
+			</div>
+			
+				<div class="message_body_right">
+		
+					<td>{{ x.board_id }}</td>
+					<b>{{ x.from }}:</b>
+					<!--<td>{{ x.type }}</td>
+					<td>{{ x.end }}</td>-->
+					<i>@{{ x.to }} </i>
+					<!--<td>{{ x.dial_id }}</td>-->
+					<td>{{ x.dial_message }}</td>
+				</div>
+			
+		</div>
+		
+		
+		
+		
+		
+		
+		
+		<div style="clear: both;"></div>
+
+	</div>
+	
 </div>
+
+
+</div>
+
 
 <script>
 var app = angular.module('myApp', []);
@@ -87,6 +216,7 @@ app.controller('customersCtrl', function($scope, $http, $interval) {
 	
 		if(($scope.names)){
 			document.getElementById("loading_sign").style.display = 'none';
+			document.getElementById("bubble_board").style.display = 'block';
 		}
 	
 	});
