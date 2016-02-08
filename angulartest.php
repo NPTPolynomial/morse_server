b1ff6edc9285bb2ab8274fbb45dca7af102973c1
NPTPolynomial <chengcao8@gmail.com>
2016-02-07 16:44:07 -0800

    added loading icon and changed layout of the board

--- angulartest.php
+++ angulartest.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
-<html >
+<html>
 <style>
+
+*{
+	margin: 0 auto;
+}
+
 table, th , td  {
   border: 1px solid grey;
   border-collapse: collapse;
@@ -12,11 +17,37 @@ table tr:nth-child(odd) {
 table tr:nth-child(even) {
   background-color: #ffffff;
 }
+
+
+.loadingImg{
+	/*background-image: url(img/hex-loader2.gif);
+	
+	animation: rotateSpinner 1.2s linear infinite;
+	
+	-webkit-animation: rotateSpinner 1.2s linear infinite;
+	display: inline-block;
+	*/
+	vertical-align: middle;
+	/*height: 24px;
+	width: 24px*/
+}
+
+#loading_sign{
+	text-align: center;
+	
+}
+
+.board_display{
+	margin-top: 30px;
+	width: 700px;
+}
+
+
 </style>
 <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 <body>
 
-<div ng-app="myApp" ng-controller="customersCtrl">
+<div class="board_display" ng-app="myApp" ng-controller="customersCtrl">
 
 <table>
 	<th>Board_id</th>
@@ -26,20 +57,25 @@ table tr:nth-child(even) {
 	<th>Dial_id</th>
 	<th>Dial_message</th>
 	
-  <tr ng-repeat="x in names">
-    <td>{{ x.board_id }}</td>
-    <td>{{ x.from }}</td>
-	<td>{{ x.type }}</td>
-	<td>{{ x.to }}</td>
-	<td>{{ x.dial_id }}</td>
-	<td>{{ x.dial_message }}</td>
-  </tr>
+	<tr>
+	<td id="loading_sign" class="loadingImg" colspan="6"><img src="img/hex-loader2.gif"></img></td>
+	</tr>
+	
+	<tr ng-repeat="x in names">
+		<td>{{ x.board_id }}</td>
+		<td>{{ x.from }}</td>
+		<td>{{ x.type }}</td>
+		<td>{{ x.to }}</td>
+		<td>{{ x.dial_id }}</td>
+		<td>{{ x.dial_message }}</td>
+	</tr>
 </table>
 
 </div>
 
 <script>
 var app = angular.module('myApp', []);
+
 app.controller('customersCtrl', function($scope, $http, $interval) {
 
 
@@ -47,7 +83,15 @@ app.controller('customersCtrl', function($scope, $http, $interval) {
     //$scope.message="This DIV is refreshed "+c+" time.";
     //c++;
     $http.get("getfeed.php")
-    .then(function (response) {$scope.names = response.data;});
+    .then(function (response) {$scope.names = response.data;
+	
+		if(($scope.names)){
+			document.getElementById("loading_sign").style.display = 'none';
+		}
+	
+	});
+	
+
 
   },10000);
 
